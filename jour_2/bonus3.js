5fbecacddd5972aa2dd84befb564df8eb365b850
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-09 15:58:34 +0100

    Ex JS Bonus 3 avec presque rien dedans -)

---
+++ jour_2/bonus3.js
@@ -0,0 +1,5 @@
+// Math.floor() donne premier entier inférieur
+// Math.ceil() donne premier entier supérieur
+// Math.round() donne premier entier inférieur ou supérieur sauf demi entier supérieur
+var roundedNumber = 3.6;
+console.log(Math.round(roundedNumber));
