166e98fba928f768faccf0b718cd72e69ac54370
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-11 15:34:52 +0100

    JS jour4 ex4

---
+++ jour_4/ex4.js
@@ -0,0 +1,15 @@
+function compare(num1, num2) {
+  if (num1 > num2) {
+    console.log('num1 is bigger than num2');
+  } else if (num1 === num2) {
+    console.log('num1 is equal to num2');
+  }else {
+    console.log('num2 is bigger than num1');}
+}
+
+
+compare(5,9);
+compare(7,5);
+compare(55,88);
+compare(22,22);
+compare(5,1);
