d5d7fa8a89fcd508e1530dcbdad5c2d173a8c7f9
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-11 17:54:05 +0100

    js jour4 ex bonus YYEESSSS

--- jour_4/bonus.js
+++ jour_4/bonus.js
@@ -1,24 +1,25 @@
 var numMin = 65;
 var numMax = 90;
-password = [];
+var word = [];
+var password = "";
+
 
 function generatePassword(num) {
   if (num < 6 || num > 15) {
     console.log('error');
     return
   }
+
   for (var i = 1; i <= num; i++) {
-    password.push(Math.floor(Math.random() * (numMax - numMin + 1)) + numMin);
-    console.log(password);
-    // console.log(typeof password);
+    word.push(Math.floor(Math.random() * (numMax - numMin + 1)) + numMin);
   }
-  for (var i = 0; i < password.length; i++) {
-    var pass = String.fromCharCode(password[i]);
-    // console.log(pass);
+
+  for (var i = 0; i < word.length; i++) {
+    password += String.fromCharCode(word[i]);
   }
-  password = pass;
-  console.log(password);
+
+  console.log(`Your password is: ${password}`);
   
 }
 
-generatePassword(9);
+generatePassword(14);
