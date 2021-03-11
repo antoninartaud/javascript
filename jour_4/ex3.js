714a57d5d25925750f702a7263d3140040396319
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-11 15:28:34 +0100

    JS jour4 ex3

---
+++ jour_4/ex3.js
@@ -0,0 +1,11 @@
+function checkIfEven(num) {
+  if (num % 2 == 0) {
+    console.log('num is even');
+  }else {
+    console.log('num is odd');}
+}
+
+checkIfEven(3);
+checkIfEven(4);
+checkIfEven(8);
+checkIfEven(13);
