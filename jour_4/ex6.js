032230f13efa1dc28b8bed414a67936d8f25375e
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-11 16:30:02 +0100

    js jour4 ex6

--- jour_4/ex6.js
+++ jour_4/ex6.js
@@ -1,3 +1,11 @@
 function format(num) {
-  
+  var heures = Math.round(num / 3600);
+  var minutes = Math.round((num % 3600) / 60);
+  var secondes = (num % 3600) % 60;
+  console.log('Heures: ', heures);
+  console.log('Minutes: ', minutes);
+  console.log('Secondes: ', secondes);
 }
+
+
+format(4524);
