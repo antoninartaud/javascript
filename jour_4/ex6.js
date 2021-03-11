ef91e7ffa0e06bd967ecec3f70d43c4abf61b514
Antonin Artaud <antoninartaud@hotmail.fr>
2021-03-11 23:24:46 +0100

    js jour4 ex5 changement méthode

--- jour_4/ex6.js
+++ jour_4/ex6.js
@@ -1,6 +1,6 @@
 function format(num) {
-  var heures = Math.round(num / 3600);
-  var minutes = Math.round((num % 3600) / 60);
+  var heures = Math.floor(num / 3600);
+  var minutes = Math.floor((num % 3600) / 60);
   var secondes = (num % 3600) % 60;
   console.log(`${heures}:${minutes}:${secondes}`);
 }
