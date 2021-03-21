911bf05c2dab0e1f0c9e2e12785839fbd787987d
antonin <antoninartaud@hotmail.fr>
2021-03-21 23:52:52 +0100

    JS jour5 ex bonus

--- jour_5/bonus.js
+++ jour_5/bonus.js
@@ -10,61 +10,89 @@
 // À l'aide du package `prompt` que vous connaissez, et du package `colors` (cherchez le sur NPM et lisez la doc), tentez d'écrire un code qui peut permettre à l'utilisateur de jouer à Motus. Si vous réussissez : [bravo !](https://youtu.be/Kz4-Z-s5R9o) Sinon : [dommage...](https://youtu.be/y_sG_lPgMvk)
 
 var prompt = require('prompt');
+var colors = require('colors');
 
-var randomWordsArray = ['disco', 'salle', 'tombe', 'devis', 'bacon', 'douze', 'cygne', 'astre', 'civet', 'panne', 'piano', 'exode', 'prise', 'atour', 'quota'];
+var randomWordsArray = ['tombe'];
 
+// ['disco', 'salle', 'tombe', 'devis', 'bacon', 'douze', 'cygne', 'astre', 'civet', 'panne', 'piano', 'exode', 'prise', 'atour', 'quota'];
+
+console.log('hello'.green);
 var minNumber = 0;
 var maxNumber = randomWordsArray.length - 1;
 
-
-var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+var randomNumber =
+  Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 // console.log(randomNumber);
 
 var mysteryWord = randomWordsArray[randomNumber];
 console.log(mysteryWord);
 
-function play() {
+// for (var i = 1; i <= 6; i++) {
+
 var mysteryWordSplit = mysteryWord.split('');
 console.log('mysteryWordSplit', mysteryWordSplit);
 
 var mysteryWordFirsLetter = mysteryWordSplit.shift();
-console.log('mysteryWordFirsLetter',mysteryWordFirsLetter);
+mysteryWordFirsLetter = mysteryWordFirsLetter.toUpperCase();
+mysteryWordFirsLetter = colors.red(mysteryWordFirsLetter);
+console.log('mysteryWordFirsLetter', mysteryWordFirsLetter);
 
 var mysteryWord4lastLetters = mysteryWordSplit;
 console.log('mysteryWord4lastLetters array', mysteryWord4lastLetters);
 
+var mysteryWord4lastLettersStr = mysteryWord4lastLetters
+  .join('')
+  .replace(/([a-zA-Z])/g, '*');
+console.log('mysteryWord4lastLetters string', mysteryWord4lastLettersStr);
 
-mysteryWord4lastLetters= mysteryWord4lastLetters.join('').replace(/([a-zA-Z])/g,'*');
-console.log('mysteryWord4lastLetters string', mysteryWord4lastLetters);
-
-var mysteryWordStars = `${mysteryWordFirsLetter}${mysteryWord4lastLetters}`;
+var mysteryWordStars = `mysteryWordStars ${mysteryWordFirsLetter}${mysteryWord4lastLettersStr}`;
 
 console.log(mysteryWordStars);
-
 
 prompt.start();
 
 var schema = {
   properties: {
     wordUser: {
-      description: 'What\'s your word ?',
+      description: "What's your word ?",
       pattern: /([a-zA-Z]){5}/g,
       required: true,
-      message: 'Choose a word with exactly 5 letters please!'
+      message: 'Choose a word with exactly 5 letters please!',
     },
-  
-  }
-}
-
+  },
+};
 
 prompt.get(schema, function (err, result) {
-  
+  var wordUserSplit = result.wordUser.split('');
+  console.log('wordUserSplit', wordUserSplit);
+
+  wordUserSplit.shift();
+  var wordUser4lastLetter = wordUserSplit;
+  console.log('wordUser4lastLetter', wordUser4lastLetter);
+
+  console.log(
+    'first letter wordUser4lastLetter',
+    colors.red(wordUser4lastLetter[0])
+  );
+
+  var wordResultColored = [];
+
+  for (var i = 0; i < mysteryWord4lastLetters.length; i++) {
+    console.log('mysteryWord4lastLetters[i]', mysteryWord4lastLetters[i]);
+    console.log('wordUser4lastLetter[i]', wordUser4lastLetter[i]);
+    if (mysteryWord4lastLetters[i] === wordUser4lastLetter[i]) {
+      var colorWordUserLetter = colors.red(wordUser4lastLetter[i]);
+      console.log('colorWordUserLetter', colorWordUserLetter);
+      console.log('colorWordUserLetter', typeof colorWordUserLetter);
+
+      wordResultColored.push(colorWordUserLetter);
+    } else {
+      wordResultColored.push('*');
+    }
+  }
+  console.log('wordResultColored', wordResultColored);
 
   console.log('Command-line input received:');
   console.log('Mot choisi: ' + result.wordUser);
   // console.log('  email: ' + result.email);
-  }
-);
-}
-
-play();
+});
