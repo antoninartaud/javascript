d045c397810b8633be0d08173df110ad0d6985d3
antonin <antoninartaud@hotmail.fr>
2021-03-22 16:54:06 +0100

    js jour7 ex6

---
+++ jour_7/ex6_cakes.js
@@ -0,0 +1,43 @@
+var cakes = [
+  {
+    name: 'cake',
+    flavor: 'vanilla',
+    status: 'available',
+  },
+  {
+    name: 'brownie',
+    flavor: 'chocolate',
+    status: 'available',
+  },
+  {
+    name: 'pie',
+    flavor: 'strawberry',
+    status: 'available',
+  },
+  {
+    name: 'muffin',
+    flavor: 'pistachio',
+    status: 'available',
+  },
+  {
+    name: 'donut',
+    flavor: 'chocolate',
+    status: 'available',
+  },
+];
+
+var cakesSoldOut = cakes.map(function (elem) {
+  if (elem.flavor === 'chocolate') {
+    elem.status = 'sold out';
+  }
+  return elem;
+});
+
+console.log(cakesSoldOut);
+
+var cakesSoldOutList = cakesSoldOut.filter(function (elem) {
+  if (elem.status === 'sold out') {
+    return elem;
+  }
+});
+console.log(cakesSoldOutList);
