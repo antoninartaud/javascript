187db2e12f7fe0faaf14e234fdf08786ac42eb43
antonin <antoninartaud@hotmail.fr>
2021-03-24 16:50:57 +0100

    JS jour8 ex2

--- jour_8/ex2_chuckNorris.js
+++ jour_8/ex2_chuckNorris.js
@@ -1,1 +1,13 @@
 var request = require('request');
+
+function getFact() {
+  request.get(
+    'https://api.chucknorris.io/jokes/random',
+    function (err, res, body) {
+      var result = JSON.parse(body);
+      console.log(result.value);
+    }
+  );
+}
+
+getFact();
