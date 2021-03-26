67f1cca6a45d5db7a49c7076ddde6d09af2b2be4
antonin <antoninartaud@hotmail.fr>
2021-03-26 17:31:22 +0100

    js rover modif

--- projet_rover/projet.js
+++ projet_rover/projet.js
@@ -1,3 +1,7 @@
+// Modules
+// var prompt = require('prompt');
+
+// Déclarations variables
 var grid = [
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
   [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -18,6 +22,7 @@ var rover = {
   travelLog: [],
 };
 
+// Déclarations fonctions
 function turnLeft(vehicle) {
   vehicle.direction = 'W';
 }
@@ -26,40 +31,61 @@ function turnRight(vehicle) {
   vehicle.direction = 'E';
 }
 
-turnRight(rover);
-// console.log(rover.direction);
-
 function moveforward(vehicle) {
-  vehicle.x++;
+  console.log('before if moveforward');
+  if (vehicle.direction === 'E') {
+    console.log('moveforwardE');
+  } else if (vehicle.direction === 'W') {
+    vehicle.y = vehicle.y + 1;
+  }
 }
-
-moveforward(rover);
-// console.log(rover.x);
 
 function pilotRover(command) {
   switch (command) {
     case 'l':
+      console.log('switch l');
+
       turnLeft(rover);
       break;
     case 'r':
+      console.log('switch r');
       turnRight(rover);
       break;
     case 'f':
+      console.log('switch f');
+
       moveforward(rover);
       break;
     default:
-      console.log('Where is Earth ?');
+      console.log('Please... i want to go back to Earth');
   }
 }
 
 function savePositionLog() {
   rover.travelLog.push(`${rover.x}, ${rover.y}`);
+  // rover.travelLog = [];
 }
 
+// Tests fonctions commandes
+// turnRight(rover);
+// moveforward(rover);
 pilotRover('f');
 
 savePositionLog();
 
 console.log('rover.direction', rover.direction);
-console.log('rover.x', rover.x);
 console.log('travelLog', rover.travelLog);
+console.log('rover.x', rover.x);
+console.log('rover.y', rover.y);
+
+// prompt.start();
+
+// // Get two properties from the user: username and email
+// prompt.get(['username', 'email'], function (err, result) {
+//   //
+//   // Log the results.
+//   //
+//   console.log('Command-line input received:');
+//   console.log('  username: ' + result.username);
+//   console.log('  email: ' + result.email);
+// });
